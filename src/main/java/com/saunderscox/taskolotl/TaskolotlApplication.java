abed737d276851e806ddb70fcfeb9bc873a8c622
Daniel Cox <Daniel.Cox@estes-express.com>
2025-04-20 19:50:21 -0400

    initial commit

---
+++ src/main/java/com/saunderscox/taskolotl/TaskolotlApplication.java
@@ -0,0 +1,13 @@
+package com.saunderscox.taskolotl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskolotlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskolotlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saunderscox/taskolotl/TaskolotlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saunderscox.taskolotl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskolotlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
